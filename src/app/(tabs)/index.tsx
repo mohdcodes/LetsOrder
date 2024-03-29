b6fec3b7389a3e3e3d9a07281591174a3718c8f1
ARBAAZcode <arbaazsid0786@gmail.com>
2024-03-29 23:53:33 +0530

    warming up with the dummy data

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,27 +1,25 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/src/components/EditScreenInfo';
+import { Image, StyleSheet } from 'react-native';
+import products from '@/assets/data/products';
 import { Text, View } from '@/src/components/Themed';
+import Colors from '@/src/constants/Colors';
+
+const product = products[1];
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>HELLO WORLD </Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Image style={styles.image} source={{ uri: product.image }} />
+      <Text style={styles.pizzahead}>{product.name}</Text>
+      <Text style={styles.price}>$12.99</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
+    padding: 15,
+    borderRadius: 20,
   },
   title: {
     fontSize: 20,
@@ -31,5 +29,18 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  pizzahead: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  price: {
+    fontSize: 18,
+    fontWeight: '600',
+    color: Colors.light.tint,
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 1,
   },
 });
