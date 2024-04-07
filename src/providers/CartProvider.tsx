e452258a0f3aa84b8c33ffecb657927969787c8c
ARBAAZcode <arbaazsid0786@gmail.com>
2024-04-07 22:21:25 +0530

    updated adding items to cart incraese quantity

--- src/providers/CartProvider.tsx
+++ src/providers/CartProvider.tsx
@@ -4,17 +4,25 @@ import { randomUUID } from 'expo-crypto';
 type CartType = {
   items: CartItem[];
   addItem: (product: Product, size: CartItem['size']) => void;
+  updateQuantity: (itemId: String, amount: -1 | 1) => void;
 };
 
 const CartContext = createContext<CartType>({
   items: [],
   addItem: () => {},
+  updateQuantity: () => {},
 });
 const CartProvider = ({ children }: PropsWithChildren) => {
   const [items, setItems] = useState<CartItem[]>([]);
   const addItem = (product: Product, size: CartItem['size']) => {
     // if already item in the cart increase the quantity
-
+    const existingItem = items.find(
+      (item) => item.product === product && item.size === size
+    );
+    if (existingItem) {
+      updateQuantity(existingItem.id, 1);
+      return;
+    }
     const newCartItem: CartItem = {
       id: randomUUID(),
       product,
@@ -25,10 +33,20 @@ const CartProvider = ({ children }: PropsWithChildren) => {
     setItems([newCartItem, ...items]);
   };
   // update quantity
-
+  const updateQuantity = (itemId: String, amount: -1 | 1) => {
+    setItems(
+      items
+        .map((item) =>
+          item.id !== itemId
+            ? item
+            : { ...item, quantity: item.quantity + amount }
+        )
+        .filter((item) => item.quantity > 0)
+    );
+  };
   console.log(items);
   return (
-    <CartContext.Provider value={{ items, addItem }}>
+    <CartContext.Provider value={{ items, addItem, updateQuantity }}>
       {children}
     </CartContext.Provider>
   );
